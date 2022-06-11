36ba5d213750e949fb6b9cfd5a0ee43ac86910b1
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-11 01:22:06 -0700

    WIP. Shapeview. add dragging support

--- src/components/Section1_Shape/ShapeView.tsx
+++ src/components/Section1_Shape/ShapeView.tsx
@@ -1,5 +1,5 @@
 import React, { HTMLAttributes, useState } from "react";
-import { useAtomValue } from "jotai";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import { editorShapeParamsAtom, viewboxOptionAtoms } from "@/store/store";
 import { generate, GeneratorResult, pointsToLines, separatePoints } from "@/store/ngon/generator";
 import { useDrag } from "@use-gesture/react";
@@ -13,20 +13,34 @@ const enum PointTyp {
     start,
 }
 
+function fn(n: number) {
+    return `${n}`.padStart(6, ' ');
+}
+
+function fn2(a: number, b: number) {
+    return `${a}`.padStart(6, ' ') + ', ' + `${b}`.padEnd(6, ' ');
+}
+
 function PointOuter({ x, y }: { x: number; y: number; }) {
+    // const setShapeParams = useSetAtom(editorShapeParamsAtom);
+    const [shapeParams, setShapeParams] = useAtom(editorShapeParamsAtom);
+
     const [isDown, setIsDown] = useState(false);
-    const bind = useDrag(({ down, target, movement: [mx, my] }) => {
+    const bind = useDrag(({ down, movement: [mx, my], memo = { x: shapeParams.outerX, y: shapeParams.outerY } }) => {
         setIsDown(down);
-        
-        //down && target && setTimeout(() => (target as SVGCircleElement).style.cursor = 'move', 0);
 
         if (!mx && !my) {
             return;
         }
 
-        console.log('delta', rnd2(mx), rnd2(my), 'x,y', x, y, target);
+        // setShapeParams((p) => ({ ...p, outerX: p.outerX + mx, outerY: p.outerX + my }));
+        setShapeParams((p) => ({ ...p, outerX: memo.x + mx, outerY: memo.y + my }));
+
+        console.log('delta', fn2(rnd2(mx), rnd2(my)), 'x,y', fn2(x, y), 'memo: ', fn2(memo.x, memo.y));
+
+        return memo;
     });
-    console.log('render');
+    isDown && console.log('render', 'x,y', fn2(x, y));
 
     return (
         <circle
