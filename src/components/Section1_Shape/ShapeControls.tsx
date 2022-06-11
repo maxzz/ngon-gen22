7d9614c08055d1082d0d002ea31f3f5c0c57d0d2
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-11 15:17:27 -0700

    WIP. ShapeControls. reduce duplicates

--- src/components/Section1_Shape/ShapeControls.tsx
+++ src/components/Section1_Shape/ShapeControls.tsx
@@ -1,5 +1,5 @@
 import React, { HTMLAttributes } from "react";
-import { PrimitiveAtom, useAtom, useSetAtom } from "jotai";
+import { PrimitiveAtom, SetStateAction, useAtom, useSetAtom } from "jotai";
 import { editorShapeParamsAtom, viewboxOptionAtoms } from "@/store/store";
 import { NewShapeParams } from "@/store/ngon/shape";
 import { initalValueShapeParams, initialValueNewShapeParamsMeta } from "@/store/ngon/shape-defaults";
@@ -145,14 +145,10 @@ function HintButton({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     );
 }
 
-export function ShapeControls({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    const [shapeParams, setShapeParams] = useAtom(editorShapeParamsAtom);
-    const { outerN, innerN, outerX, outerY, innerX, innerY, stroke, w, h, ofsX, ofsY, scale, } = shapeParams;
+type NewShapeParamsNumbers = Omit<NewShapeParams, 'id' | 'genId' | 'swap'>;
 
-    const shapeMembers = { outerN, innerN, outerX, outerY, innerX, innerY, stroke, };
-    const sceneMembers = { w, h, ofsX, ofsY, scale, };
-
-    const shapeControls = Object.entries(shapeMembers).map(([key, val]) => {
+function ControlsGroup({ members, setShapeParams }: { members: Partial<NewShapeParamsNumbers>; setShapeParams: (update: SetStateAction<NewShapeParams>) => void; }) {
+    const shapeControls = Object.entries(members).map(([key, val]) => {
         const meta = initialValueNewShapeParamsMeta[key as keyof Omit<NewShapeParams, 'id' | 'genId' | 'swap'>];
         return meta && (
             <NewSlider
@@ -168,22 +164,49 @@ export function ShapeControls({ className, ...rest }: HTMLAttributes<HTMLDivElem
             />
         );
     });
+    return (<>{shapeControls}</>);
+}
 
-    const sceneControls = Object.entries(sceneMembers).map(([key, val]) => {
-        const meta = initialValueNewShapeParamsMeta[key as keyof Omit<NewShapeParams, 'id' | 'genId' | 'swap'>];
-        return meta && (
-            <NewSlider
-                label={meta.label}
-                min={meta.min}
-                max={meta.max}
-                step={meta.step}
-                linkWithNext={meta.link}
-                onChange={(value) => setShapeParams((p) => ({ ...p, [key]: meta.digits === 0 ? Math.floor(value) : value }))}
-                value={val}
-                key={key}
-            />
-        );
-    });
+export function ShapeControls({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
+    const [shapeParams, setShapeParams] = useAtom(editorShapeParamsAtom);
+    const { outerN, innerN, outerX, outerY, innerX, innerY, stroke, w, h, ofsX, ofsY, scale, } = shapeParams;
+
+    const shapeMembers = { outerN, innerN, outerX, outerY, innerX, innerY, stroke, };
+    const sceneMembers = { w, h, ofsX, ofsY, scale, };
+
+
+    // const shapeControls = Object.entries(shapeMembers).map(([key, val]) => {
+    //     const meta = initialValueNewShapeParamsMeta[key as keyof Omit<NewShapeParams, 'id' | 'genId' | 'swap'>];
+    //     return meta && (
+    //         <NewSlider
+    //             label={meta.label}
+    //             min={meta.min}
+    //             max={meta.max}
+    //             step={meta.step}
+    //             linkWithNext={meta.link}
+    //             onChange={(value) => setShapeParams((p) => ({ ...p, [key]: meta.digits === 0 ? Math.floor(value) : value }))}
+    //             value={val}
+    //             key={key}
+    //             title={meta.hint}
+    //         />
+    //     );
+    // });
+
+    // const sceneControls = Object.entries(sceneMembers).map(([key, val]) => {
+    //     const meta = initialValueNewShapeParamsMeta[key as keyof Omit<NewShapeParams, 'id' | 'genId' | 'swap'>];
+    //     return meta && (
+    //         <NewSlider
+    //             label={meta.label}
+    //             min={meta.min}
+    //             max={meta.max}
+    //             step={meta.step}
+    //             linkWithNext={meta.link}
+    //             onChange={(value) => setShapeParams((p) => ({ ...p, [key]: meta.digits === 0 ? Math.floor(value) : value }))}
+    //             value={val}
+    //             key={key}
+    //         />
+    //     );
+    // });
 
     return (
         <div className={classNames("px-2 py-4 text-xs bg-primary-200 flex flex-col space-y-2 cursor-default", className)} {...rest}>
@@ -191,13 +214,15 @@ export function ShapeControls({ className, ...rest }: HTMLAttributes<HTMLDivElem
             {/* <Separator label="Shape" /> */}
 
             <div className="">
-                {shapeControls}
+                {/* {shapeControls} */}
+                <ControlsGroup members={shapeMembers} setShapeParams={setShapeParams} />
             </div>
 
             <Separator label="Box" />
 
             <div className="">
-                {sceneControls}
+                {/* {sceneControls} */}
+                <ControlsGroup members={sceneMembers} setShapeParams={setShapeParams} />
             </div>
 
             <div className="flex justify-between">
