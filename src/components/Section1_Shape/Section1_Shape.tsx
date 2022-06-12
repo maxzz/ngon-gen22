a914a2d5e6613537e324a150c03efe22c3f02eca
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-11 17:39:02 -0700

    WIP. ShapeViewText. add save generated path

--- src/components/Section1_Shape/Section1_Shape.tsx
+++ src/components/Section1_Shape/Section1_Shape.tsx
@@ -1,9 +1,11 @@
-import { a, easings, useSpring } from '@react-spring/web';
-import { ShapeView } from './ShapeView';
-import { ShapeControls } from './ShapeControls';
+import { HTMLAttributes } from 'react';
 import { useAtomValue } from 'jotai';
 import { editorShapeAtom, editorShapeParamsAtom } from '@/store/store';
 import { generateSvg, pointsToLines } from '@/store/ngon/generator';
+import { a, easings, useSpring } from '@react-spring/web';
+import { ShapeView } from './ShapeView';
+import { ShapeControls } from './ShapeControls';
+import { classNames } from '@/utils/classnames';
 
 const previewBoxShadow = { boxShadow: '0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12)', };
 
@@ -18,6 +20,22 @@ function ShapeViewContainer() {
     );
 }
 
+function SaveButton({ className, ...rest }: HTMLAttributes<HTMLInputElement>) {
+    return (
+        <input
+            className={classNames(
+                "px-1 py-0.5 border-primary-400 border-dotted border rounded-sm shadow-sm",
+                "bg-primary-200 hover:bg-primary-300 focus:bg-primary-300",
+                "outline-none focus:ring-1 ring-offset-1 ring-offset-primary-50 ring-primary-700/50",
+                "active:scale-[.97] cursor-pointer",
+                className
+            )}
+            type="button"
+            value="Copy" {...rest}
+        />
+    );
+}
+
 function ShapeViewText() {
     const shapeParams = useAtomValue(editorShapeParamsAtom);
     const shape = useAtomValue(editorShapeAtom);
@@ -25,10 +43,14 @@ function ShapeViewText() {
     const generated = generateSvg(path.join(''), shapeParams.w, shapeParams.h, shapeParams.stroke);
     return (
         <div className="px-2 py-1 h-32 text-xs bg-primary-100 border-primary-300 border overflow-hidden" style={{ ...previewBoxShadow }}>
+
+            <SaveButton onClick={async () => {
+                navigator.clipboard.writeText(generated);
+            }} />
+
             <div className="h-full overflow-overlay">
-                <div className="pr-4">
-                    {generated}
-                </div>
+                <textarea className="w-full bg-inherit outline-none" rows={5} readOnly spellCheck="false" value={generated}>
+                </textarea>
             </div>
         </div>
     );
