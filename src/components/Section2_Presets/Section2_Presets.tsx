837665d53f394440afa86767dc07a13602bf9520
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-14 19:26:32 -0700

    WIP. Section2_Presets. add presets render

--- src/components/Section2_Presets/Section2_Presets.tsx
+++ src/components/Section2_Presets/Section2_Presets.tsx
@@ -1,13 +1,13 @@
 import React, { HTMLAttributes } from 'react';
 import { useAtomValue } from 'jotai';
-import { editorShapeAtom, editorShapeParamsAtom, openSections, vaultData,  } from '@/store/store';
+import { openSections, vaultData, } from '@/store/store';
+import { NewShapeParams } from '@/store/ngon/shape';
+import { IO, StorageNgon } from '@/store/ngon/shape-io';
+import { generate, GeneratorResult } from '@/store/ngon/generator';
 import { UISection } from '../UI/UISection';
 import { classNames } from '@/utils/classnames';
-import { IO, StorageNgon } from '@/store/ngon/shape-io';
 
-export function ShapeView({ className, ...rest }: HTMLAttributes<SVGSVGElement>) {
-    const shapeParams = useAtomValue(editorShapeParamsAtom);
-    const shape = useAtomValue(editorShapeAtom);
+export function ShapeView({ shapeParams, shape, className, ...rest }: { shapeParams: NewShapeParams, shape: GeneratorResult; } & HTMLAttributes<SVGSVGElement>) {
     return (
         <svg
             className={classNames("fill-transparent touch-none bg-primary-50 border-white border-4", className)}
@@ -24,17 +24,21 @@ function ShapePresets() {
     const shapes = useAtomValue(vaultData.shapesAtom);
     const shapeParamArray = shapes.map((shapeStr) => {
         const p = JSON.parse(shapeStr) as StorageNgon;
-        const newShape = IO.ShapeNgonFromStorage(p);
-    })
+        const shapeParams = IO.ShapeNgonFromStorage(p);
+        const shape = generate(shapeParams);
+        return {
+            shapeParams,
+            shape,
+        };
+    });
     return (
         <div className="py-2">
             <div className="max-h-96 px-4 overflow-y-auto bg-primary-100">
                 Gallery
                 <div className="py-4 grid grid-cols-[repeat(auto-fill,minmax(64px,1fr))] gap-1">
-                    <ShapeView />
-                    <ShapeView />
-                    <ShapeView />
-                    <ShapeView />
+                    {shapeParamArray.map(({ shapeParams, shape }, idx) => (
+                        <ShapeView shapeParams={shapeParams} shape={shape} key={idx} />
+                    ))}
                 </div>
             </div>
         </div>
