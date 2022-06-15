c27542c9cf366e8bd5c5ab8da87f13d2506a9da2
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-14 17:50:17 -0700

    WIP. Section2_Presets. add shape preset prototype

--- src/components/Section2_Presets/Section2_Presets.tsx
+++ src/components/Section2_Presets/Section2_Presets.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 import { useAtomValue } from 'jotai';
-import { releaseNotesAtom, section1_OpenAtom } from '../../store/store';
+import { editorShapeAtom, editorShapeParamsAtom, releaseNotesAtom, section1_OpenAtom } from '../../store/store';
 import { UISection } from '../UI/UISection';
+import { classNames } from '@/utils/classnames';
+
+export function ShapeView({ className, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    const shapeParams = useAtomValue(editorShapeParamsAtom);
+    const shape = useAtomValue(editorShapeAtom);
+    return (
+        <svg
+            className={classNames("fill-transparent touch-none bg-primary-50 border-white border-4", className)}
+            viewBox={`0 0 ${shapeParams.w} ${shapeParams.h}`}
+            preserveAspectRatio="none"
+            {...rest}
+        >
+            <path className="stroke-primary-900" style={{ strokeWidth: shapeParams.stroke }} d={shape.d} />
+        </svg>
+    );
+}
+
 
 function ShapePresets() {
     const releaseNotes = useAtomValue(releaseNotesAtom);
     return (
         <div className="py-2">
-            <div className="notes max-h-96 px-4 overflow-y-auto bg-slate-100">
+            <div className="max-h-96 px-4 overflow-y-auto bg-primary-100">
                 Gallery
+                <div className="py-4 grid grid-cols-[repeat(auto-fill,minmax(64px,1fr))] gap-1">
+                    <ShapeView />
+                    <ShapeView />
+                    <ShapeView />
+                    <ShapeView />
+                </div>
             </div>
         </div>
     );
 }
-
 
 export function Section2_Presets() {
     return (
