4e0e33a658e9f92284fb2c2ae34e1662377d5148
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-17 17:17:30 -0700

    WIP. store. switch to VaultSpapesArray. 3. remove old implementation of vaultData

--- src/components/Section2_Presets/Section2_Presets.tsx
+++ src/components/Section2_Presets/Section2_Presets.tsx
@@ -1,13 +1,10 @@
-import React, { HTMLAttributes, useEffect, useState } from 'react';
+import React, { HTMLAttributes } from 'react';
 import { useAtomValue, useSetAtom } from 'jotai';
-import { editorShapeParamsAtom, openSections, vaultData, } from '@/store/store';
+import { editorShapeParamsAtom, openSections, vaultSpapesArray, } from '@/store/store';
 import { NewShapeParams } from '@/store/ngon/shape';
-import { IO } from '@/store/ngon/shape-io';
 import { GeneratorResult } from '@/store/ngon/generator';
 import { UISection } from '../UI/UISection';
 import { classNames } from '@/utils/classnames';
-import { isNonNull } from '@/utils/tsX';
-import { toastError } from '../UI/UiToaster';
 
 export function ShapeView({ shapeParams, shape, className, ...rest }: { shapeParams: NewShapeParams, shape: GeneratorResult; } & HTMLAttributes<SVGSVGElement>) {
     const setShapeParams = useSetAtom(editorShapeParamsAtom);
@@ -25,41 +22,12 @@ export function ShapeView({ shapeParams, shape, className, ...rest }: { shapePar
 }
 
 function ShapePresets() {
-    //const shapes = useAtomValue(vaultData.shapesAtom);
-    
-    // const { parsedShapes, failedShapes } = useAtomValue(vaultParsedshapesAtom);
-    // useEffect(() => {
-    //     failedShapes.forEach((shape) => toastError(shape));
-    // }, [failedShapes]);
-
-    const shapes = useAtomValue(vaultData.shapesAtom);
-    const [shapeParamArray, setShapeParamArray] = useState<IO.ConvertResult[]>([]);
-    useEffect(() => {
-        const arr = shapes.map((shapeStr) => {
-            const res = IO.shapeFromString(shapeStr);
-            if (typeof res === 'string') {
-                toastError(res); //TODO: we can now use faildedShapesAtom if need it for later.
-            } else {
-                return res;
-            }
-        }).filter(isNonNull);
-        setShapeParamArray(arr);
-    }, [shapes]);
-
-    // const shapeParamArray = shapes.map((shapeStr) => {
-    //     const res = IO.shapeFromString(shapeStr);
-    //     if (typeof res === 'string') {
-    //         toastError(res);
-    //     } else {
-    //         return res;
-    //     }
-    // }).filter(isNonNull);
-
+    const shapes = useAtomValue(vaultSpapesArray.validAtom);
     return (
         <div className="py-2">
             <div className="max-h-96 px-4 overflow-y-auto bg-primary-100">
                 <div className="py-4 grid grid-cols-[repeat(auto-fill,minmax(64px,1fr))] gap-1">
-                    {shapeParamArray.map(({ shapeParams, shape }, idx) => (
+                    {shapes.map(({ shapeParams, shape }, idx) => (
                         <ShapeView shapeParams={shapeParams} shape={shape} key={idx} />
                     ))}
                 </div>
