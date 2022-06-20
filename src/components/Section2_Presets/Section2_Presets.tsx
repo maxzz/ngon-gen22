a39cf360ccfcbef5ac3f268aa9a0fc3b6a29b7e3
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-20 14:09:56 -0700

    WIP. Section2_Presets. experimants with dragging element styles. 2

--- src/components/Section2_Presets/Section2_Presets.tsx
+++ src/components/Section2_Presets/Section2_Presets.tsx
@@ -12,12 +12,13 @@ import { classNames } from '@/utils/classnames';
 function PresetView({ shapeParams, shape }: { shapeParams: NewShapeParams, shape: GeneratorResult; }) {
     const setShapeParams = useSetAtom(editorShapeParamsAtom);
     return (
-        <div className="relative group hover:scale-[1.5] transition-all z-0 hover:z-10 [.sortable-chosen]:text:red-700">
-            <IconCross className={classNames("absolute m-px w-4 h-4 right-px top-px p-0.5 hidden group-hover:block text-red-500 bg-red-100 border-red-300/75 border rounded",
-            ""
+        <div className="relative group hover:scale-105 transition-all z-0 hover:z-10">
+            <IconCross className={classNames(
+                "absolute m-px w-4 h-4 right-1 top-1 p-0.5 hidden group-hover:block text-red-900 hover:bg-red-100 border-red-300/75 hover:border rounded",
+                "",
             )} />
 
-            <PreviewBox className="text-inherit group-hover:bg-primary-200" shapeParams={shapeParams} shape={shape} onClick={() => setShapeParams(shapeParams)} />
+            <PreviewBox className="text-inherit group-hover:bg-primary-200 [&:.sortable-chosen]:bg-green-300" shapeParams={shapeParams} shape={shape} onClick={() => setShapeParams(shapeParams)} />
         </div>
     );
 }
@@ -27,7 +28,7 @@ function ShapePresets() {
     return (
         <div className="py-2">
             <div className="max-h-96 px-4 overflow-y-auto bg-primary-100">
-                <ReactSortable list={shapes} setList={setShapes} className="relative py-4 grid grid-cols-[repeat(auto-fill,minmax(64px,1fr))] gap-1" animation={150}>
+                <ReactSortable list={shapes} setList={setShapes} className="relative py-4 grid grid-cols-[repeat(auto-fill,minmax(64px,1fr))] gap-1" animation={200}>
                     {shapes.map(({ id, shapeParams, shape }) => (
                         <Fragment key={id}>
                             <PresetView shapeParams={shapeParams} shape={shape} />
