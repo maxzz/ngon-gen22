ef0e4a815f1f785f5a9bf9100938d54946b156ad
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-22 18:01:03 -0700

    WIP. app. sync names with the new section. 2

--- src/components/App2_Main.tsx
+++ src/components/App2_Main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { App1_Header } from './App1_Header';
 import { App3_Footer } from './App3_Footer';
-import { Section2_Source } from './Section2_Source/Section11_Source';
+import { Section2_Source } from './Section2_Source/Section2_Source';
 import { Section1_Editor } from './Section1_Editor/Section1_Editor';
-import { Section3_Presets } from './Section3_Presets/Section2_Presets';
-import { Section4_Multiplication } from './Section4_Multiplication/Section3_Multiplication';
+import { Section3_Presets } from './Section3_Presets/Section3_Presets';
+import { Section4_Multiplication } from './Section4_Multiplication/Section4_Multiplication';
 
 export function App2_Main() {
     return (
@@ -23,7 +23,7 @@ export function App2_Main() {
                     <Section1_Editor />
                     <Section2_Source />
                     <Section3_Presets />
-                    <Section4_Multiplication />
+                    {/* <Section4_Multiplication /> */}
                 </div>
             </div>
 
