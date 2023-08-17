a172fd947a01f42ea1c4196fc980056685843cb6
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-17 13:52:24 -0700

    WIP. move non-main sections to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useAtomValue } from 'jotai';
 import { dataLoadAtom } from './store/store';
-import { App2_Main } from './components/App2_Main';
+import { App1_Header } from './components/app1-header';
+import { App2_Main } from './components/app2-main';
+import { App3_Footer } from './components/app3-footer';
 import { UIToaster } from './components/UI/UiToaster';
 import { UISymbolsDefs } from './components/UI/Icons/UIIconSymbols';
 import './App.scss';
@@ -24,8 +26,13 @@ function App() {
         <PopperRoot />
         <UISymbolsDefs />
         <DataLoader />
+        
         <div className="min-h-full overflow-hidden bg-slate-50">
-            <App2_Main />
+            <div className="h-screen flex flex-col">
+                <App1_Header />
+                <App2_Main />
+                <App3_Footer />
+            </div>
         </div>
     </>);
 }
--- src/components/app2-main.tsx
+++ src/components/app2-main.tsx
@@ -1,33 +1,24 @@
-import React from 'react';
-import { App1_Header } from './app1-header';
-import { App3_Footer } from './app3-footer';
-import { Section2_Source } from './sections/2-source';
 import { Section1_Editor } from './sections/1-editor';
+import { Section2_Source } from './sections/2-source';
 import { Section3_Presets } from './sections/3-presets';
 import { Section4_Multiplication } from './sections/4-multiplication';
 
 export function App2_Main() {
     return (
-        <div className="h-screen flex flex-col">
-            <App1_Header />
+        <div className="flex-1 overflow-y-auto" style={{ overflow: 'overlay' }}>
+            {/* <div className="m-auto w-4/5 flex flex-col space-y-4"> */}
+            {/* <div className="m-auto max-w-[80vw] flex flex-col space-y-4"> */}
+            {/* <div className="m-auto max-w-[80%] flex flex-col space-y-4"> */}
+            {/* <div className="m-auto max-w-[80%] flex flex-col space-y-4" style={{ overflow: 'hidden', scrollbarGutter: 'stable' }}> */}
+            {/* <div className="ml-[calc(20vw/2)] mr-[calc(calc(20vw/2)-16px)] max-w-3xl flex flex-col space-y-4"> */}
+            {/* <div className="ml-[calc(20vw/2)] mr-auto max-w-3xl flex flex-col space-y-4"> */}
 
-            <div className="flex-1 overflow-y-auto" style={{ overflow: 'overlay' }}>
-                {/* <div className="m-auto w-4/5 flex flex-col space-y-4"> */}
-                {/* <div className="m-auto max-w-[80vw] flex flex-col space-y-4"> */}
-                {/* <div className="m-auto max-w-[80%] flex flex-col space-y-4"> */}
-                {/* <div className="m-auto max-w-[80%] flex flex-col space-y-4" style={{ overflow: 'hidden', scrollbarGutter: 'stable' }}> */}
-                {/* <div className="ml-[calc(20vw/2)] mr-[calc(calc(20vw/2)-16px)] max-w-3xl flex flex-col space-y-4"> */}
-                {/* <div className="ml-[calc(20vw/2)] mr-auto max-w-3xl flex flex-col space-y-4"> */}
-
-                <div className="py-4 mx-auto max-w-[29rem] sm:max-w-[37rem] md:max-w-2xl lg:max-w-3xl flex flex-col space-y-4 transition-all">
-                    <Section1_Editor />
-                    <Section2_Source />
-                    <Section3_Presets />
-                    <Section4_Multiplication />
-                </div>
+            <div className="py-4 mx-auto max-w-[29rem] sm:max-w-[37rem] md:max-w-2xl lg:max-w-3xl flex flex-col space-y-4 transition-all">
+                <Section1_Editor />
+                <Section2_Source />
+                <Section3_Presets />
+                <Section4_Multiplication />
             </div>
-
-            <App3_Footer />
         </div>
     );
 }
