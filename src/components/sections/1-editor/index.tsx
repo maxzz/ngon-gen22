b677bee3e4a60bd349fdc8593451f280c7cf4ef1
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-17 14:26:17 -0700

    WIP. update editor layout

--- src/components/sections/1-editor/index.tsx
+++ src/components/sections/1-editor/index.tsx
@@ -1,11 +1,15 @@
 import { a, easings, useSpring } from '@react-spring/web';
-import { EditorShape } from './1-shape-view';
-import { EditorControls } from './2-shape-controls';
+import { EditorShape } from './1-left-view';
+import { EditorControls } from './2-right-controls';
 
 export const previewBoxShadow = { boxShadow: '0 1px 1px -1px #00000018, 0 1px 1px 0 #00000024, 0 1px 3px 0 #0000001f', };
 
 function ShapeViewContainer() {
-    const styles = useSpring({ scale: 1, from: { scale: .2 }, config: { duration: 2000, easing: easings.easeInOutElastic } });
+    const styles = useSpring({
+        scale: 1,
+        from: { scale: .2 },
+        config: { duration: 2000, easing: easings.easeInOutElastic },
+    });
     return (
         <div className="bg-slate-100 aspect-square border-primary-300 border overflow-hidden" style={{ ...previewBoxShadow, transition: "all .2s" }}>
             <a.div style={styles} className="w-full h-full object-cover">
